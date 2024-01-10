d4f280db78d569288dda222e33874c310f2b29cc
Anastasija <anastasija.krstanoska@students.finki.ukim.mk>
2024-01-10 12:04:32 +0100

    Initial commit

---
+++ blog/resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!doctortype html>
+
+<title>My blg</title>
+<link rel="stylesheet" href="/app.css">
+<title>My blog</title>
+
+<body>
+
+<article>
+    <?= $post; ?>
+
+</article>
+
+<a href="/">Go back</a>
+
+</body>
